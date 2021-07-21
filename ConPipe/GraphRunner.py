4384ca6dce5aaba4b8e461e1335c9134eb95ff68
Laouen <laouen.belloli@gmail.com>
2021-07-21 21:47:29 +0000

    Implemented build and run graph without save and reload from disk feature

--- ConPipe/GraphRunner.py
+++ ConPipe/GraphRunner.py
@@ -1,9 +1,9 @@
+import graph
 import yaml
 from graph import Graph
 
-from ConPipe.DataSplit import DataSplit
-from ConPipe.ModelSelection import ModelSelection
-from ConPipe.ModelEvaluation import ModelEvaluation
+from ConPipe import modules
+from ConPipe.utils import find_function_from_modules
 
 # Function to load yaml configuration file
 def load_config(config_path):
@@ -12,32 +12,23 @@ def load_config(config_path):
 
     return config
 
+# TODO: agregar los modulos del usuario
+graph_modules = [modules]
+
+def get_node_module(funct_name):
+    return find_function_from_modules(
+        graph_modules,
+        funct_name
+    )
+
 
 class GraphRunner():
 
     def __init__(self, config):
         self.config = config
-
+        self.verbose = self.config['general']['verbose']
+        
         self._load_graph_nodes()
-
-        self.data_preprocess = DataProcess(self.config['data_preprocess'])
-        self.feature_extraction = FeatureExtraction(self.config['feature_extraction'])
-        self.data_augmentation = DataAugmentation(self.config['data_augmentation'])
-
-        self.data_split = DataSplit(
-            self.config['evaluation_schema'],
-            self.config['general']['verbose']
-        )
-        
-        self.model_selection = ModelSelection(
-            self.config['model_selection'],
-            self.config['general']['verbose']
-        )
-        
-        self.model_evaluation = ModelEvaluation(
-            self.config['model_evaluation']
-            self.config['general']['verbose']
-        )
 
     def _load_graph_nodes(self):
 
@@ -45,21 +36,47 @@ class GraphRunner():
         self.graph_ = Graph()
 
         for name, config in self.config.items():
-            if name !== 'general':
-                self.graph_.add_node(name, {'config': config})
+            if name != 'general':
+                self.graph_.add_node(
+                    name, 
+                    {
+                        **config,
+                        'module': get_node_module(config['function'])(
+                            config['parameters'],
+                            self.verbose
+                        )
+                    }
+                )
 
         # Build DAG graph
         self.root_nodes_ = []
-        for curr_node in self.graph_nodes_:
-            prev_nodes = [
-                node 
-                for node in self.graph_nodes_ 
-                if node['name'] in curr_node['config']['input_from']
-            ]
-            
-            if len(prev_nodes) > 0:
-                curr_node.relate(prev_nodes, 'INPUT_FROM')
-            else:
+        for curr_node in self.graph_.nodes():
+            input_from = self.graph_.node(curr_node)['input_from']
+        
+            if len(input_from) == 0:
                 self.root_nodes_.append(curr_node)
 
-    def run()
+            for node in input_from:
+                self.graph_.add_edge(curr_node, node)
+        
+    def run(self):
+
+        # TODO: load output from disk
+
+        for curr_node in self.graph_.topological_sort():
+
+            if self.verbose > 1:
+                print(f'executing node {curr_node}')
+            
+            # Collect inputs from dependent nodes
+            inputs = {} 
+            for node in self.graph_.node(curr_node)['input_from']:
+                inputs.update(self.graph_.node(node)['output'])
+            
+            # Compute output and save result in graph
+            output = self.graph_.node(node)['module'].run(**inputs)
+            self.graph_.node(node)['output'] = output
+
+            # TODO: save output to disk
+
+
