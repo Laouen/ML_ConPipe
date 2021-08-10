76780148f136d079c8f4422c1c1756277e0276d4
Laouen <laouen.belloli@gmail.com>
2021-08-10 20:23:25 +0000

    minor bug fix if function has no parameters

--- ConPipe/FunctionModule.py
+++ ConPipe/FunctionModule.py
@@ -1,7 +1,7 @@
 class FunctionModule():
 
     def __init__(self, function, parameters):
-        self.parameters = parameters
+        self.parameters = parameters if parameters is not None else {}
         self.function = function
 
     def run(self, *args, **kwargs):
